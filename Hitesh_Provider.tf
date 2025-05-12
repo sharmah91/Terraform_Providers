725de6d295b63e4a75fc949e63c38fac61b1fa4a
sharmah91 <sharmah91@gmail.com>
2025-05-12 13:45:27 +0530

    First Upload

---
+++ Hitesh_Provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "5d5be31f-46ae-4693-866a-7cb188220117"
+}
